ffdfe8e52bdb09c2f6b6e53418f779c858eb651f
abyanji <a.byanjil99@gmail.com>
2022-12-05 01:31:02 +0300

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+int DataEntrySquare(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+void FindXY(int square)
+{
+    if (square == 1) Console.WriteLine("x > 0 and y > 0");
+    if (square == 2) Console.WriteLine("x < 0 and y > 0");
+    if (square == 3) Console.WriteLine("x < 0 and y < 0");
+    if (square == 4) Console.WriteLine("x > 0 and y < 0");
+    if (square < 1 || square > 4) Console.WriteLine("Недопустимое значение");
+}
+
+int square = DataEntrySquare("Введите номер четверти = ");
+FindXY(square);
