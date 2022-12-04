8892341ca31f3038439aa1fb1a3b09fb65c3ec53
abyanji <a.byanjil99@gmail.com>
2022-12-05 01:11:34 +0300

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+int DataEntryXY(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+void FindQuandrat(int x, int y)
+{
+    if (x > 0 && y > 0) Console.WriteLine("Это первый квадрат");
+    if (x < 0 && y > 0) Console.WriteLine("Это второй квадрат");
+    if (x < 0 && y < 0) Console.WriteLine("Это третий квадрат");
+    if (x > 0 && y < 0) Console.WriteLine("Это четвертый квадрат");
+}
+
+
+int x = DataEntryXY("Введите координаты x = ");
+// Console.WriteLine(x);
+int y = DataEntryXY("Введите координаты y = ");
+// Console.WriteLine(y);
+
+FindQuandrat(x, y);
