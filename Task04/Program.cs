cd1ea4239d56ea3143017cfebde261d6c920581c
abyanji <a.byanjil99@gmail.com>
2022-12-05 03:31:47 +0300

    Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+int NewNumber(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+void SquareTable(int N)
+{
+    int index = 1;
+    while (index < N+1)
+    {
+        double square = Math.Pow(index, 2);
+        Console.Write($" {square}");
+        index++;
+    }
+}
+int number = NewNumber($"Таблица квадратов чисел до числа ");
+SquareTable(number);
